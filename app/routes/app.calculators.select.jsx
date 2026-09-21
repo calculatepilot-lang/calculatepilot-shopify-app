025540b4e1517e4632ebc43158727ff40d38dd27
Calculate Pilot <calculatepilot@gmail.com>
2026-09-22 01:17:59 +0500

    Fix SecurityError when redirecting to theme editor (window.top.location.assign blocked cross-origin; use window.open with _top instead)

--- app/routes/app.calculators.select.jsx
+++ app/routes/app.calculators.select.jsx
@@ -90,7 +90,7 @@ export default function CalculatorSelectionRoute() {
   const { themeEditorUrl, calculatorName } = useLoaderData();
 
   useEffect(() => {
-    window.top.location.assign(themeEditorUrl);
+    window.open(themeEditorUrl, "_top");
   }, [themeEditorUrl]);
 
   return (
